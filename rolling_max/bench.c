8c664385f25eccf1c8b07bbf669dfa707ee8d836
Ludovic Andrieu <ludovic.andrieu@etu.u-bordeaux.fr>
2022-10-03 18:14:43 +0200

    linux compatible

--- rolling_max/bench.c
+++ rolling_max/bench.c
@@ -8,7 +8,8 @@
     #include <sys/resource.h>
 
     double perf_counter() {
-        struct timeval t, tzp;
+        struct timeval t;
+        struct timezone tzp;
         gettimeofday(&t, &tzp);
         return t.tv_sec + t.tv_usec*1e-6;
     }
